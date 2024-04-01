0f69baccbe77caefdcc69ee3be51d8d8a05bc915
Sudarshan kakde <60603199+sudarshankakde@users.noreply.github.com>
2024-04-01 22:57:13 +0530

    project commiy

--- movie_recommendation_system/settings.py
+++ movie_recommendation_system/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-n!tt5^(gx=pd1jwc7s_ts2)lu5azuxkk@lqvnsdgmbhe-oe41d"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['sudarshankakde.pythonanywhere.com','localhost','127.0.0.1']
 
 
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = "movie_recommendation_system.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ['templates'],
+        "DIRS": [os.path.join(BASE_DIR,'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -75,14 +75,14 @@ WSGI_APPLICATION = "movie_recommendation_system.wsgi.app"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 # settings.py
-DATABASES = {}
+# DATABASES = {}
 
 
 # Password validation
